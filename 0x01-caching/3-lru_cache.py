3e1a1d3e8d0a2697f858fed9bde3ea2e0b063943
victormayowa <hardmanvicky01@gmail.com>
2024-01-31 12:41:00 -0800

    lru

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" LRUCache module """
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache class that inherits from BaseCaching """
+
+    def __init__(self):
+        """ Initialize LRUCache """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = self.order.pop(0)
+                del self.cache_data[discarded_key]
+                print("DISCARD: {}".format(discarded_key))
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key in self.order:
+            self.order.remove(key)
+            self.order.append(key)
+        return self.cache_data.get(key, None)
